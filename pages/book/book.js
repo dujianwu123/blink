585c67a4a90c60517a8ec431256b78d7cab9d453
dujianwu123 <dujianwu123@126.com>
2019-10-21 16:49:09 +0800

    book页面的样式

--- pages/book/book.js
+++ pages/book/book.js
@@ -8,7 +8,7 @@ Page({
    * 页面的初始数据
    */
   data: {
-    
+    books: []
   },
 
   /**
@@ -17,7 +17,9 @@ Page({
   onLoad: function (options) {
     const hotList = bookApi.getHotList()
     hotList.then((res)=>{
-      console.log(res)
+      this.setData({
+        books: res
+      })
     })
   },
 
