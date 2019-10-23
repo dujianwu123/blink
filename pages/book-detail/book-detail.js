cb7ddddc42defc81dc09866b164fcb3c00ce21e3
dujianwu123 <dujianwu123@126.com>
2019-10-23 16:51:19 +0800

    将book详情页获取数据用Promise.all改写，并加入loading效果

--- pages/book-detail/book-detail.js
+++ pages/book-detail/book-detail.js
@@ -5,7 +5,6 @@ import {
 const bookApi = new bookModel();
 let likeApi = new likeModel();
 Page({
-
   /**
    * 页面的初始数据
    */
@@ -21,6 +20,7 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
+    wx.showLoading();
     // 获取id
     const bid = options.bid; // 接收外部传递过来的数据
     console.log(bid);
@@ -28,25 +28,34 @@ Page({
     const comments = bookApi.getComments(bid);
     const likeStatus = bookApi.getLikeStatus(bid);
 
-    detail.then(res=>{
-      console.log('detail',res)
+    Promise.all([detail, comments, likeStatus]).then((res)=>{
       this.setData({
-        book: res
-      })
-    });
-    comments.then(res=>{
-      console.log('comments', res)
-      this.setData({
-        comments: res.comments
-      })
-    });
-    likeStatus.then(res=>{
-      console.log('likeStatus', res)
-      this.setData({
-        likeStatus: res.like_status,
-        likeCount: res.fav_nums
-      })
+        book: res[0],
+        comments: res[1].comments,
+        likeStatus: res[2].like_status,
+        likeCount: res[2].fav_nums
+      });
+      wx.hideLoading();
     })
+    // detail.then(res=>{
+    //   console.log('detail',res)
+    //   this.setData({
+    //     book: res
+    //   })
+    // });
+    // comments.then(res=>{
+    //   console.log('comments', res)
+    //   this.setData({
+    //     comments: res.comments
+    //   })
+    // });
+    // likeStatus.then(res=>{
+    //   console.log('likeStatus', res)
+    //   this.setData({
+    //     likeStatus: res.like_status,
+    //     likeCount: res.fav_nums
+    //   })
+    // });
   },
   onLike: function (event) {
     const like_or_status = event.detail.behavior;
