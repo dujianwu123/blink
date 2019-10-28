7df3a9ac78ecb8600359bb8b6db807f89e3eaee6
dujianwu123 <dujianwu123@126.com>
2019-10-28 10:21:36 +0800

    my 页面获取用户信息来显示头像

--- pages/my/my.js
+++ pages/my/my.js
@@ -5,16 +5,44 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    authorized: false,
+    userInfo: null
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    this.userAuthorized()
   },
-
+  userAuthorized() {
+    // 获取用户是否授权
+    wx.getSetting({
+      success: data=>{
+        console.log(data);
+        if(data.authSetting['scope.userInfo']) {
+          wx.getUserInfo({
+            success: data => {
+              this.setData({
+                authorized: true,
+                userInfo: data.userInfo
+              })
+            }
+          })
+        } else {
+        }
+      }
+    })
+  },
+  onGetUserInfo(event) {
+    const userInfo = event.detail.userInfo;
+    if(userInfo) {
+      this.setData({
+        userInfo,
+        authorized: true
+      })
+    }
+  },
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
